c1f4fcecedc6a11d24ddec490c12fdb9bee77dd2
Jan Jakub Zábranský <zabransky@SPSEJECNANET.CZ>
2023-03-09 08:37:47 +0100

    commit 9.3.

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 const reqBtn = document.getElementById("reqButton");
+const tbl = document.getElementById('table');
 reqBtn.style.visibility = 'hidden';
 
-$.ajax({
-    type: "GET",
-    url: "https://api.coingecko.com/api/v3/ping",
-    success: function(data) {
-            console.log("Server is up");
-            $("#status").text("Server is up");
-    }
-    
-});
+reqBtn.addEventListener('click', Connect);
+
+function Connect() {
+    $.ajax({
+        type: "GET",
+        url: "https://api.coingecko.com/api/v3/ping",
+        success: function(data) {
+                console.log("Server is up");
+                $("#status").text("Server is up");
+                coiner();
+        },
+        error: function(error) {
+            console.log("Server is down");
+            $("#status").text("Server is down")
+            reqBtn.style.visibility = 'visible';
+        }
+    });
+}
+
+Connect();
+
 
 function coiner() {
-    $.get("https://api.coingecko.com/api/v3/coins/list")
+    $(tbl).append('<tr><th>Name</th><th>Price</th></tr>');
+    $.get("https://api.coingecko.com/api/v3/coins/list");
 }
